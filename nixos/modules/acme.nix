66db9e7e89ed325461d05ed2afd858406bc24c09
Ivan Dobriy <65816379+ForgivenID@users.noreply.github.com>
2025-08-14 23:11:28 +0300

    Update acme.nix
    Added group field and gave prosody access to domain's certs.
    Added a tempdir for acme challenges.

--- nixos/modules/acme.nix
+++ nixos/modules/acme.nix
@@ -6,16 +6,19 @@ in
     acceptTerms = true;
     defaults = {
       email = "zornfeuer@i2pmail.org";
+      webroot = "/var/lib/acme/acme-challenges";
     };
-    certs = {
-      "boltanka.tech" = {
-        inherit domain;
-        webroot = "/var/www/${domain}";
-        extraDomainNames = [
-          "conference.${domain}"
-          "upload.${domain}"
-        ];
-      };
+    certs.${domain} = {
+      extraDomainNames = [
+        "conference.${domain}"
+        "upload.${domain}"
+      ];
+      group = "prosody"; # Даём Prosody доступ.
     };
   };
+
+  # Создаём директорию для валидации.
+  systemd.tmpfiles.rules = [
+    "d /var/lib/acme/acme-challenges 0755 acme acme -"
+  ];
 }
