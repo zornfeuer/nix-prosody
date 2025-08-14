331856fa4771f23778b15c25abd02562ca25d377
Ivan Dobriy <65816379+ForgivenID@users.noreply.github.com>
2025-08-14 23:14:16 +0300

    Update prosody.nix
    Hotfix

--- nixos/modules/prosody.nix
+++ nixos/modules/prosody.nix
@@ -1,28 +1,33 @@
 let
   domain = "test.boltanka.tech";
+  certDir = "/var/lib/acme/${domain}";
 in
 {
   services.prosody = {
     enable = true;
     admins = [ "admin@${domain}" ];
-    ssl.cert = "/var/lib/acme/${domain}/fullchain.pem";
-    ssl.key = "/var/lib/acme/${domain}/key.pem";
+    ssl.cert = "${certDir}/full.pem";  # Опечатка? fullchain->full
+    ssl.key = "${certDir}/key.pem";
     allowRegistration = true;
 
-    virtualHosts = {
-      boltanka = {
-        enabled = true;
-        inherit domain;
-        ssl.cert = "/var/lib/acme/${domain}/fullchain.pem";
-        ssl.key = "/var/lib/acme/${domain}/key.pem";
+    virtualHosts.${domain} = {  # Опечатка? boltanka->test.boltanka
+      enabled = true;
+      domain = "${domain}";
+      ssl = {
+        cert = "${certDir}/full.pem";
+        key = "${certDir}/key.pem";
       };
     };
+
     muc = [
       {
         name = "ChatRooms on ${domain}";
         domain = "conference.${domain}";
       }
     ];
-    uploadHttp.domain = "uploads.${domain}";
+    uploadHttp.domain = "upload.${domain}";
   };
+
+  # Даём Prosody доступ.
+  users.users.prosody.extraGroups = [ "acme" ];
 }
